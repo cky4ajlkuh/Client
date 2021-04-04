52e98d0840b7bd7624df34c250588713a44ad8a6
Konstantinov <misha.tank.2014@mail.ru>
2021-04-05 01:30:31 +0400

    потихоньку барагозим

--- src/main/java/MyClient.java
+++ src/main/java/MyClient.java
@@ -2,7 +2,15 @@ import java.io.*;
 import java.net.*;
 
 public class MyClient {
+
+    private final static char x = 'X';
+    private final static char o = 'O';
+    private final static char zero = 'H';
+    private final static int size = 3;
+    private final static char[][] field = new char[size][size];
+
     public static void main(String[] args) throws IOException {
+        /*
         Socket client = new Socket("127.0.0.1", 9999);
         BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
@@ -23,6 +31,48 @@ public class MyClient {
 
         writer.close();
         reader.close();
-        client.close();
+        client.close();*/
+        initialization();
+        Sout(field);
+    }
+
+    public static void Sout(char[][] field) {
+        for (char[] chars : field) {
+            for (int j = 0; j < field.length; j++) {
+                System.out.print(chars[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void initialization() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                field[i][j] = zero;
+            }
+        }
+    }
+
+    private void setX(int x, int y, char value) {
+        checkBorders(x, y);
+        field[x][y] = value;
+    }
+
+    private boolean checkBorders(int x, int y) {
+        if (x < size || y < size) {
+            if (x > 0 || y > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean invalidValue(int x, int y) {
+        if (field[x][y] == o | field[x][y] == MyClient.x) {
+            return false;
+        } else if (field[x][y] == zero) {
+            return true;
+        }
+        return false;
     }
 }
